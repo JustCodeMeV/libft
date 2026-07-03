c635ebee2b4aa7291aaf79bb877957a125f7ac9a
Victor Rouxel <vrouxel@c2r5s1.42warsaw.pl>
2026-07-03 18:15:04 +0200

    strnstr, clear and secured :)

--- libft.h
+++ libft.h
@@ -6,17 +6,18 @@
 /*   By: vrouxel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/17 14:24:30 by vrouxel           #+#    #+#             */
-/*   Updated: 2026/07/03 17:21:45 by vrouxel          ###   ########.fr       */
+/*   Updated: 2026/07/03 18:11:54 by vrouxel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_strlen(const char *s);
-int	ft_isprint(int c);
-int	ft_isascii(int c);
-int	ft_isalnum(int c);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_strlen(const char *s);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
+int		ft_isalnum(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
---
+++ src/ft_strnstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vrouxel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/03 17:45:35 by vrouxel           #+#    #+#             */
+/*   Updated: 2026/07/03 18:13:20 by vrouxel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!little || *little == '\0')
+		return ((char *)big);
+	else if (!big || *big == '\0')
+		return (NULL);
+	i = 0;
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] == little[j] && (i + j) < len && little[j])
+			j++;
+		if (!little[j])
+			return ((char *)&big[i]);
+		i++;
+	}
+	return (NULL);
+}
