21e46b1b257058a4e76fcf348062b4be1627f89e
Victor Rouxel <vrouxel@c2r5s1.42warsaw.pl>
2026-07-03 18:26:50 +0200

    strlcat

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -1,24 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vrouxel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/17 14:24:30 by vrouxel           #+#    #+#             */
-/*   Updated: 2026/07/03 18:20:46 by vrouxel          ###   ########.fr       */
+/*   Created: 2026/07/03 18:16:10 by vrouxel           #+#    #+#             */
+/*   Updated: 2026/07/03 18:26:19 by vrouxel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stddef.h>
+#include "../libft.h"
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_strlen(const char *s);
-int		ft_isprint(int c);
-int		ft_isascii(int c);
-int		ft_isalnum(int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	if (!dst || !src)
+		return (0);
+	while (i < size && dst[i])
+	{
+		i++;
+	}
+	while ((i + j + 1) < size && src[j])
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (i != size)
+	{
+		dst[i + j] = '\0';
+	}
+	return (i + ft_strlen(src));
+}
