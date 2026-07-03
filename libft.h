c891b5b8bcef9fdcba0b74f454846cbc3edf3d6b
Victor Rouxel <vrouxel@c2r5s1.42warsaw.pl>
2026-07-03 18:53:18 +0200

    strchr and strrchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vrouxel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/17 14:24:30 by vrouxel           #+#    #+#             */
-/*   Updated: 2026/07/03 18:29:10 by vrouxel          ###   ########.fr       */
+/*   Updated: 2026/07/03 18:38:08 by vrouxel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
@@ -23,3 +23,5 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
