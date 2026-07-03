9ad87f841b74c21ff02bc3f8b9d22ffd428f55e3
Victor Rouxel <vrouxel@c2r5s1.42warsaw.pl>
2026-07-03 16:21:05 +0200

    gitignore, isalnum, isprint, cleared others

--- libft.h
+++ libft.h
@@ -6,10 +6,13 @@
 /*   By: vrouxel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/17 14:24:30 by vrouxel           #+#    #+#             */
-/*   Updated: 2026/06/17 14:24:34 by vrouxel          ###   ########.fr       */
+/*   Updated: 2026/07/03 16:11:56 by vrouxel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(char *str);
+int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_strlen(const char *s);
+int	ft_isprint(int c);
+int	ft_isascii(int c);
+int	ft_isalnum(int c);
